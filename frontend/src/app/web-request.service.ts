1b8994d838016beabf8b3f8fc8b698bbf54d2120
yashilbusa <yashilpatel1914@gmail.com>
2025-02-25 14:44:24 +0530

    commit

--- frontend/src/app/web-request.service.ts
+++ frontend/src/app/web-request.service.ts
@@ -6,6 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class WebRequestService {
 
+  private apiUrl = "http://localhost:3030"
   constructor(private http:HttpClient) { }
+
+  get(url:any){
+    return this.http.get(`${this.apiUrl}/${url}`)
+  }
+
+  post(url:any,payload:any){
+    return this.http.post(`${this.apiUrl}/${url}`,payload)
+  }
 
 }
