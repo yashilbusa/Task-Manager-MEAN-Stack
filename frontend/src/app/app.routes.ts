ef17d8ee63e427f626af76e0887bbb0494ff9743
yashilbusa <yashilpatel1914@gmail.com>
2025-02-25 15:38:07 +0530

    commit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { NewListComponent } from './pages/new-list/new-list.component';
 
 export const routes: Routes = [
     {
         path: '', component:TaskViewComponent
+    },
+    {
+        path: 'newlist', component:NewListComponent
     }
 ];
---
+++ frontend/src/app/pages/new-list/new-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-list.component.html',
+  styleUrl: './new-list.component.css'
+})
+export class NewListComponent {
+
+}
