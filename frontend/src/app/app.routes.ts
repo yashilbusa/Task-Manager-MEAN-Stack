3754d9c89bda54cc8bf7b4ca629857b53204634c
yashilbusa <yashilpatel1914@gmail.com>
2025-02-25 10:44:34 +0530

    commit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component:TaskViewComponent
+    }
+];
---
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-view.component.html',
+  styleUrl: './task-view.component.css'
+})
+export class TaskViewComponent {
+
+}
