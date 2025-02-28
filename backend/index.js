84b119715984c91a8a84d79dbfca3532eee95cb0
yashilbusa <yashilpatel1914@gmail.com>
2025-02-28 16:47:59 +0530

    c

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express()
 const port = 6060
 dotenv.config()
 
-app.use(cors({origin: "https://task-manager-rho-wheat.vercel.app/lists"}));
+app.use(cors({origin: "https://task-manager-rho-wheat.vercel.app/lists" ,credentials:true}));
 app.use(express.json())
 app.use(bodyparser.json())
 
