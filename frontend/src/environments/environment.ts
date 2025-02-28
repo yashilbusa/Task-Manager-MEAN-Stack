566b8618358bb6c27f844417c39516c170eb6356
yashilbusa <yashilpatel1914@gmail.com>
2025-02-28 16:43:18 +0530

    commit

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    backendUrl: "http://localhost:6060"
+    backendUrl: "https://task-manager-mean-stack-backend.vercel.app/"
+    // backendUrl: "http://localhost:6060"
 };
